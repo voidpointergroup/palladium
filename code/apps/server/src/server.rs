5211b764a8be729ee65ca1ca3e3bb51acb7901bf
replicadse <aw@voidpointergroup.com>
2022-10-11 19:43:58 -0500

    services

--- code/apps/server/src/server.rs
+++ code/apps/server/src/server.rs
@@ -3,6 +3,8 @@ use std::{
     str::FromStr,
 };
 
+use redis::Commands;
+
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub struct DirectiveACLs {
@@ -17,6 +19,7 @@ pub struct Directive {
     pub acls: DirectiveACLs,
 }
 
+#[derive(Debug, Clone)]
 pub struct Server {
     #[allow(dead_code)]
     config: crate::config::Config,
@@ -33,13 +36,21 @@ impl Server {
         }
     }
 
+    fn key_root(&self) -> String {
+        "palladium:directive".to_owned()
+    }
+
     fn key(&self, id: &str) -> String {
-        format!("palladium:directive:{}", id)
+        format!("{}:{}", self.key_root(), id)
     }
 
     pub async fn register(&self, directive: Directive) -> Result<String, Box<dyn Error>> {
         let id = uuid::Uuid::new_v4().to_string();
+        self.register_with_id(id.clone(), directive).await?;
+        Ok(id)
+    }
 
+    pub async fn register_with_id(&self, id: String, directive: Directive) -> Result<(), Box<dyn Error>> {
         let mut redis_command = &mut redis::pipe();
         redis_command = redis_command
             .cmd("SET")
@@ -54,7 +65,37 @@ impl Server {
             | None => {},
         }
         redis_command.query::<()>(&mut self.redis.get_connection()?)?;
-        Ok(id)
+        Ok(())
+    }
+
+    pub async fn list(&self) -> Result<Vec<String>, Box<dyn Error>> {
+        let prefix = format!("{}:", self.key_root());
+        let pattern = format!("{}{}", prefix, "*");
+        let keys = self
+            .redis
+            .get_connection()?
+            .scan_match::<String, String>(pattern)?
+            .map(|d| d.trim_start_matches(&prefix).to_owned())
+            .collect();
+        Ok(keys)
+    }
+
+    pub async fn delete(&self, id: &str) -> Result<(), Box<dyn Error>> {
+        redis::pipe()
+            .cmd("DEL")
+            .arg(self.key(&id))
+            .query::<()>(&mut self.redis.get_connection()?)?;
+        Ok(())
+    }
+
+    pub async fn clear(&self) -> Result<(), Box<dyn Error>> {
+        let mut pipe = &mut redis::pipe();
+        let keys = self.list().await?;
+        for k in keys {
+            pipe = pipe.cmd("DEL").arg(self.key(&k))
+        }
+        pipe.query::<()>(&mut self.redis.get_connection()?)?;
+        Ok(())
     }
 
     pub async fn redirect(&self, id: String) -> Result<Directive, Box<dyn Error>> {
