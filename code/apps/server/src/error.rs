edf3440881b9e779abae1ca55e71a2d173b596c5
replicadse <aw@voidpointergroup.com>
2022-10-30 22:46:00 +0100

    mongodb

--- code/apps/server/src/error.rs
+++ code/apps/server/src/error.rs
@@ -1,3 +1,4 @@
+palladium_error::make_error!(Runtime);
 palladium_error::make_error!(UnknownCommandError);
 palladium_error::make_error!(MissingArgumentError);
 palladium_error::make_error!(StartupError);
